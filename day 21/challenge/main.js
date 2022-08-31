a357e2c6d83f96079973302dbfab192682ee29c4
Palskie <57343545+plskz@users.noreply.github.com>
2022-08-31 12:12:47 +0000

    feat: add day 21

---
+++ day 21/challenge/main.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('nav--visible');
+});
